3b217e60b174b37d6470bc1c0b446a4c74792a8b
liashevchenko <scarydoor99@gmail.com>
2023-11-06 19:13:58 +0100

    hw-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,25 @@
-
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+let player;
 
-player.on('timeupdate', _.throttle(currentTimeRunner, 1000));
+function initializePlayer() {
+  player = new Vimeo.Player(iframe);
+  player.on('timeupdate', _.throttle(currentTimeRunner, 1000));
 
-function currentTimeRunner(data) {
+  function currentTimeRunner(data) {
     const currentTime = data.seconds;
     localStorage.setItem("videoplayer-current-time", currentTime);
-    
-}
+  }
 
-
-const savedTime = localStorage.getItem("videoplayer-current-time");
-if (savedTime) {
+  const savedTime = localStorage.getItem("videoplayer-current-time");
+  if (savedTime) {
     player.setCurrentTime(parseFloat(savedTime)).then(function () {
+      if (document.hasFocus()) {
         player.play();
+      }
     });
+  }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  initializePlayer();
+});
